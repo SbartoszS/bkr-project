c367ad5dfde252add1bf95c72cb85e45950c1bf4
BStojek <es.bartek@gmail.com>
2020-09-15 10:43:13 +0200

    three section footer, cta ,innumbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { CtaComponent } from './cta/cta.component';
+import { NumbersComponent } from './numbers/numbers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    CtaComponent,
+    NumbersComponent
   ],
   imports: [
     BrowserModule
