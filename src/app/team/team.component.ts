d5bd130b5deaf95be563518e159130854d8863e1
BStojek <es.bartek@gmail.com>
2020-09-15 22:07:14 +0200

    finish carousel

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -8,21 +8,29 @@ declare var $: any;
   styleUrls: ['./team.component.scss'],
 })
 export class TeamComponent implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    $('.owl-carousel').owlCarousel({
+      autoplay: false,
+      autoplayHoverPause: true,
+      slideSpeed: 300,
+      paginationSpeed: 500,
+      items: 3,
+      dots: false,
+      loop: true,
+      nav: true,
+      navText: ['<span>&#10229;</span>', '<span>&#10230;</span>'],
+      responsive: {
+        0: {
+          items: 1,
+          autoplay: false,
+        },
+        768: {
+          items: 2,
+        },
+        1100: {
+          items: 3,
+        },
+      },
+    });
+  }
 }
-
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function () {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i = 0; i < 4; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
