43938c645a63fe02bf5d5919a21a1af22e84805e
BStojek <es.bartek@gmail.com>
2020-09-16 08:39:34 +0200

    fix merge

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -10,19 +10,3 @@ declare var $: any;
 export class TeamComponent implements OnInit {
   ngOnInit(): void {}
 }
-
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function () {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i = 0; i < 4; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
